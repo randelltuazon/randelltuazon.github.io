ff00f6817c1044cc7087c019015c0426fb4b013b
Randell Tuazon <randelltuazon@gmail.com>
2023-02-19 09:07:32 +0800

    Enhanced homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,34 +7,30 @@ import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import PKG from '../../package.json';
 import styles from './index.module.css';
 
-function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons + ' mt-4'}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/getting-started"
-          >
-            Get Started ⏱️
-          </Link>
-        </div>
-        <div className="mt-4">Currently <strong>v{PKG.version}</strong></div>
-      </div>
-    </header>
-  );
-}
-
 export default function Home(): JSX.Element {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
+
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
       description="Description will go into a meta tag in <head />">
-      <HomepageHeader />
+
+      <header className="patterns-hero-bank-note py-8 lg:py-16 text-center relative overflow-hidden dark:bg-gray-800">
+        <div className="container">
+          <h1 className="text-6xl font-light mb-4">{siteConfig.title}</h1>
+          <p className="text-xl font-light mb-8">{siteConfig.tagline}</p>
+          <div className={clsx('flex', styles.buttons)}>
+            <Link
+              className="btn btn-lg"
+              to="/docs/getting-started"
+            >
+              Get Started ⏱️
+            </Link>
+          </div>
+          <div className="mt-4">Currently <strong>v{PKG.version}</strong></div>
+        </div>
+      </header>
+
       <main>
         <HomepageFeatures />
       </main>
